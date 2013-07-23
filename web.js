a6b6d286ec6e85a91139fa54d0f4391abce8eed9
Elizabeth <burdenelizabeth@gmail.com>
2013-07-23 02:46:40 +0000

    Fix bug

--- web.js
+++ web.js
@@ -2,15 +2,13 @@ var express = require('express');
 var fs = require('fs');
 var buffer = new Buffer(30);
 
-buffer = fs.readFileSync(index.html)
-
-var thetext = buffer.toString();
+buffer = fs.readFileSync(index.html);
 
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(thetext);
+  response.send(buffer.toString);
 });
 
 var port = process.env.PORT || 5000;
