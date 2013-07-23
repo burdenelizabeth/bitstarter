ddb364abd0e64c8f07004154b6c917ca33c33ab1
Elizabeth <burdenelizabeth@gmail.com>
2013-07-23 02:02:27 +0000

    Read from file

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fs = require('fs');
+var buffer = new Buffer(30);
+
+buffer = fs.readFileSync(index.html)
+
+var thetext = buffer.toString();
+
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(thetext);
 });
 
 var port = process.env.PORT || 5000;
